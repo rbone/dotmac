cbd23e1c19e4fa65667558beca403900395ea784
Richard Bone <github@accounts.rjb.pink>
2018-09-18 14:57:10 +1000

    Add shim for npm to work around nvm slowness
    This adds the same kind of shim that I've added for nvm and node for npm
    this time. I also tweaked the code a bit to be a bit more reusable, and
    help avoid certain issues with figuring out what is loaded when.

--- packages/node/init.sh
+++ packages/node/init.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
 export NVM_DIR="$HOME/.nvm"
-function nvm() {
-  source "/usr/local/opt/nvm/nvm.sh"
-  nvm "$@"
-
+function load_nvm() {
   unset -f node
+  unset -f npm
+
+  source "/usr/local/opt/nvm/nvm.sh"
+}
+function nvm() {
+  load_nvm
+
+  nvm "$@"
 }
 
 function node() {
-  nvm > /dev/null 2>&1
+  load_nvm
 
   node "$@"
+}
+
+function npm() {
+  load_nvm
+
+  npm "$@"
 }
